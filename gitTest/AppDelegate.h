ccba8ab439fee8a33a98a60dc978b44a4f509eb1
Omnipresent <omnipresent@Saurabhsinh-Rathod-iMac.local>
2015-06-30 15:31:18 +0530

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Omnipresent on 30/06/15.
+//  Copyright (c) 2015 Omnipresent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Omnipresent on 30/06/15.
+//  Copyright (c) 2015 Omnipresent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
